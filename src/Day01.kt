f20b5981126cdb257f42a5e531f2fabac3aed38c
Martijn van der Blom <martijn.van.der.blom@mn.nl>
2021-12-06 17:01:18 +0100

    Solution for part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+
+    fun part1(input: List<String>): Int = input.subList(1, input.size)
+        .zip(input)
+        .map { (curr, prev) -> if (curr.toInt() > prev.toInt()) "increased" else "decreased" }
+        .count { it.equals("increased") }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testOutputPart1 = part1(testInput)
+    println(testOutputPart1)
+    check(testOutputPart1 == 7)
 
     val input = readInput("Day01")
-    println(part1(input))
+    val outputPart1 = part1(input)
+    println(outputPart1)
+    check(outputPart1 == 1298)
+
     println(part2(input))
 }
