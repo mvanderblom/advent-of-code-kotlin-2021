1643cb27378197b08003e261680403a8c4e66744
Martijn van der Blom <martijn.van.der.blom@mn.nl>
2021-12-07 10:05:52 +0100

    Adds testcase for day 1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -22,6 +22,10 @@ fun main() {
     println(outputPart1)
     check(outputPart1 == 1298)
 
+    val testOutputPart2 = part2(testInput)
+    println(testOutputPart2)
+    check(testOutputPart2 == 5)
+
     val outputPart2 = part2(input)
     println(outputPart2)
     check(outputPart2 == 1248)
