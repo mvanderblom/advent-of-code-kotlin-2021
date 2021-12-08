ea18a19eba23880e1f8cc196a89936c2d206bc21
Martijn van der Blom <martijn@vanderblom.dev>
2021-12-08 20:53:15 +0100

    Solution day 4 part 1

---
+++ src/Day04.kt
@@ -0,0 +1,94 @@
+fun main() {
+    fun List<List<Int>>.transpose(): List<List<Int>> {
+        val columns = this[0].size
+
+        val transposed = (0 until columns).map { mutableListOf<Int>() }.toMutableList()
+        for (row in this) {
+            for (column in 0 until columns) {
+                transposed[column].add(row[column])
+            }
+        }
+
+        return transposed
+    }
+
+    class Board(rawData: List<String>){
+        val rows: List<List<Int>>
+        val cols: List<List<Int>>
+        val numbers: List<Int>
+
+        init {
+            this.rows = rawData.map { row ->
+                row.chunked(3)
+                    .map { number ->
+                        number.trim().toInt()
+                    }
+            }
+
+            this.cols = this.rows.transpose()
+            this.numbers = this.rows.flatten()
+        }
+
+        fun score(drawnNumbers: List<Int>): Int {
+            val winningRow by lazy { this.rows.any { row -> drawnNumbers.containsAll(row)  } }
+            val winningCol by lazy { this.cols.any { col -> drawnNumbers.containsAll(col)  } }
+
+            if(winningRow || winningCol) {
+                return (this.numbers - drawnNumbers).sum() * drawnNumbers.last()
+            }
+
+            return -1
+        }
+
+        override fun toString(): String {
+            return "Board" + System.lineSeparator() +
+                    cols.joinToString(System.lineSeparator()) +
+                    System.lineSeparator()
+        }
+    }
+
+
+    fun part1(input: List<String>): Int {
+        val boards = input
+            .subList(2,input.size)
+            .filter { it != "" }
+            .chunked(5)
+            .map(::Board)
+        println("boards: ${boards}")
+
+        val randomNumbers = input[0]
+            .split(',')
+            .map { it.toInt() }
+        println("randomNumbers: ${randomNumbers}")
+
+        for (i in (0..randomNumbers.size)) {
+            for (board in boards) {
+                val score = board.score(randomNumbers.subList(0, i+1))
+                if (score > -1)
+                    return score
+            }
+        }
+
+        return -1
+    }
+
+    fun part2(input: List<String>): Int = input.size
+
+    val testInput = readInput("Day04_test")
+    val testOutputPart1 = part1(testInput)
+    println(testOutputPart1)
+    check(testOutputPart1 == 4512)
+
+    val input = readInput("Day04")
+    val outputPart1 = part1(input)
+    println(outputPart1)
+    check(outputPart1 == 44088)
+
+//    val testOutputPart2 = part2(testInput)
+//    println(testOutputPart2)
+//    check(testOutputPart2 == 5)
+//
+//    val outputPart2 = part2(input)
+//    println(outputPart2)
+//    check(outputPart2 == 1248)
+}
