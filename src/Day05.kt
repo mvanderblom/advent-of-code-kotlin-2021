7b926cae1ddad39d776f99f6abb26e297cd6f110
Martijn van der Blom <martijn.van.der.blom@mn.nl>
2021-12-09 16:59:51 +0100

    removes testcode

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,11 +1,3 @@
-import java.lang.Exception
-
-fun rangeBetween(thiz: Int, that: Int): IntRange {
-    if (thiz < that)
-        return thiz..that
-    return that..thiz
-}
-
 data class Point(val x: Int, val y: Int) {
     companion object {
         fun of(stringValue: String): Point {
@@ -55,6 +47,12 @@ data class Line(val start: Point, val end: Point) {
     }
 }
 
+fun rangeBetween(thiz: Int, that: Int): IntRange {
+    if (thiz < that)
+        return thiz..that
+    return that..thiz
+}
+
 fun main() {
 
     fun part1(input: List<String>, includeDiagonals: Boolean = false): Int {
@@ -66,38 +64,6 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int = part1(input, true)
-
-
-    checkLine(
-        "1,1 -> 3,3", listOf(
-            Point(1, 1),
-            Point(2, 2),
-            Point(3, 3)
-        )
-    )
-
-    checkLine(
-        "3,3 -> 1,1", listOf(
-            Point(3, 3),
-            Point(2, 2),
-            Point(1, 1)
-        )
-    )
-
-    checkLine(
-        "8,0 -> 0,8", listOf(
-            Point(8, 0),
-            Point(7, 1),
-            Point(6, 2),
-            Point(5, 3),
-            Point(4, 4),
-            Point(3, 5),
-            Point(2, 6),
-            Point(1, 7),
-            Point(0, 8)
-        )
-    )
-
 
     val testInput = readInput("Day05_test")
     val testOutputPart1 = part1(testInput)
@@ -116,17 +82,4 @@ fun main() {
     val outputPart2 = part2(input)
     println(outputPart2)
     check(outputPart2 == 18605)
-}
-
-private fun checkLine(line: String, expectedPoints: List<Point>) {
-    val actualPoints = Line.of(line).getPoints(true)
-    println("Checking: $line")
-    println("Actual: $actualPoints")
-    println("Expected: $expectedPoints")
-    try{
-        check(actualPoints.containsAll(expectedPoints))
-        println("Yes!")
-    } catch (e: Exception){
-        println("FAILFAIL")
-    }
 }
