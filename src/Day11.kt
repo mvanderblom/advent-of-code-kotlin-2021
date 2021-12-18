1a995873eaeb5d18a701e5aa2fe43a3bcae70c7d
Martijn van der Blom <martijn@vanderblom.dev>
2021-12-19 00:58:11 +0100

    Day 11 OO implementation feels better

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,87 +1,106 @@
-fun main() {
-    val dayName = "Day11"
+class Octopus(
+    var energy: Int,
+    val grid: Grid
+) {
+    private var hasFlashed = false
+    lateinit var neighbours: List<Octopus>
 
-    fun getNeighbours(point: Pair<Int, Int>, input: List<List<Int>>) =
-        listOf(-1 to 0, 0 to -1, 0 to 1, 1 to 0, -1 to -1, 1 to 1, -1 to 1, 1 to -1)
+    fun executeStep() {
+        energy++
+        if(energy == 10) {
+            flash()
+        }
+    }
+
+    fun flash() {
+        if(!hasFlashed) {
+            energy = 0
+            grid.countFlash()
+            neighbours.forEach { it.executeStep() }
+        }
+    }
+
+    fun reset() {
+        hasFlashed = false
+    }
+}
+
+class Grid {
+    var flashCount = 0
+    private val gridMap: MutableMap<Pair<Int, Int>, Octopus> = mutableMapOf()
+    private val grid: List<List<Octopus>>
+
+    constructor(input: List<String>) {
+        this.grid = input.mapIndexed { rowIndex, row ->
+            row
+                .toList()
+                .mapIndexed { colIndex, c ->
+                    val energy = c.toString().toInt()
+                    val octopus = Octopus( energy, this)
+                    gridMap.put(rowIndex to colIndex, octopus)
+                    octopus
+                }
+        }
+
+        gridMap.entries.forEach { (coord, octo)->
+            octo.neighbours = getNeighbours(coord)
+        }
+    }
+
+    private fun getNeighbours(coord: Pair<Int, Int>): List<Octopus> {
+        return listOf(-1 to 0, 0 to -1, 0 to 1, 1 to 0, -1 to -1, 1 to 1, -1 to 1, 1 to -1)
             .mapNotNull { (addRow, addCol) ->
-                val (rowIndex, colIndex) = point
+                val (rowIndex, colIndex) = coord
                 val neighbourRow = rowIndex + addRow
                 val neighbourCol = colIndex + addCol
 
-                if (neighbourRow >= 0 && neighbourRow < input.size && neighbourCol >= 0 && neighbourCol < input[0].size)
-                    neighbourRow to neighbourCol
+                if (neighbourRow >= 0 && neighbourRow < grid.size && neighbourCol >= 0 && neighbourCol < grid[0].size)
+                    gridMap[neighbourRow to neighbourCol]
                 else
                     null
             }
-
-    fun updateNeighbours(point: Pair<Int, Int>, input: List<MutableList<Int>>) {
-        getNeighbours(point, input)
-            .forEach { (rowIndex, colIndex) ->
-                input[rowIndex][colIndex]++
-            }
     }
-    fun printBoard(input: List<List<Int>>) {
-        println(input.joinToString(System.lineSeparator()) { it.map { it.toString().padStart(2) }.joinToString()})
-    }
-    fun findFlashes(input: List<List<Int>>) = input
-        .mapIndexed { rowIndex, row ->
-            row.mapIndexed { colIndex, value ->
-                if (value >= 10)
-                    rowIndex to colIndex
-                else
-                    null
-            }.filterNotNull()
-        }.flatten()
 
-    fun executeStep(input: List<MutableList<Int>>): Int {
-        input.forEachIndexed { rowIndex, row ->
-            row.forEachIndexed { colIndex, _ ->
-                input[rowIndex][colIndex]++
-            }
+    fun executeStep(): Int {
+        gridMap.values.forEach {
+            it.reset()
+            it.executeStep()
         }
-        println("Updated every point by 1")
-        var flashCount = 0
-        do {
-            println("Looking for flashes in: ")
-            printBoard(input)
-
-            val flashes = findFlashes(input)
-            println("flashes(${flashes.size}) $flashes")
-
-            flashes.forEach { (rowIndex, colIndex) ->
-                flashCount += input[rowIndex][colIndex] % 9
-                input[rowIndex][colIndex] = 0
-                updateNeighbours(rowIndex to colIndex, input)
-            }
-        }
-        while (flashes.isNotEmpty())
-
         return flashCount
     }
 
-    fun part1(input: List<String>, steps: Int = 100): Int {
-        val rows = input.map { row ->
-            row.toList()
-                .map { it.toString().toInt() }
-                .toMutableList()
-        }
-        println("Before any steps:")
-        printBoard(rows)
+    fun print() {
+        println(grid.joinToString(System.lineSeparator()) { it.map { it.energy.toString().padStart(2) }.joinToString()})
+    }
 
-        return (1..steps).sumOf { i ->
+    fun countFlash() {
+        flashCount++
+    }
+
+}
+
+fun main() {
+    val dayName = "Day11"
+
+    fun part1(input: List<String>, steps: Int = 100): Int {
+        val grid = Grid(input)
+        println("Before any steps:")
+        grid.print()
+
+        (1..steps).forEach{ i ->
             println("Executing step $i: ")
-            val flashes = executeStep(rows)
+            grid.executeStep()
             println("After step $i: ")
-            printBoard(rows)
-            flashes
+            grid.print()
         }
+        return grid.flashCount
     }
 
     fun part2(input: List<String>): Int = input.size
 
-//    val testInputSimple = readInput("${dayName}_test_simple")
-//    val x = part1(testInputSimple, 2)
-//    x isEqualTo 9
+    val testInputSimple = readInput("${dayName}_test_simple")
+    val x = part1(testInputSimple, 2)
+    x isEqualTo 9
 
     val testInput = readInput("${dayName}_test")
     val input = readInput(dayName)
