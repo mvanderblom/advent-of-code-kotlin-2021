018cad7a8d42cfd3fc123d41c26f8c3ca8924ed8
Martijn van der Blom <martijn@vanderblom.dev>
2021-12-18 14:03:04 +0100

    Refactors solution day 9 part 1 to be more usefull for part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,29 +1,93 @@
 fun main() {
     val dayName = "Day09"
 
-    fun part1(input: List<String>): Int {
-        val rows = input.map { it.toList().map { it.toString().toInt() } }
-        val lastRow = rows.size - 1
-        val lastCol = rows[0].size - 1
-
-        var lowPoints = 0
-        rows
-            .forEachIndexed { rowNum, values ->
-                values.forEachIndexed { colNum, value ->
-                    val up = if (rowNum > 0) rows[rowNum-1][colNum] else Int.MAX_VALUE
-                    val down = if (rowNum < lastRow) rows[rowNum+1][colNum] else Int.MAX_VALUE
-                    val left = if (colNum > 0) rows[rowNum][colNum-1] else Int.MAX_VALUE
-                    val right = if (colNum < lastCol) rows[rowNum][colNum+1] else Int.MAX_VALUE
-
-                    if ( value < up && value < down && value < left && value < right) {
-                        lowPoints += 1 + value
-                    }
-                }
-            }
-        return lowPoints
+    fun List<Int>.product(): Int {
+        if(isEmpty())
+            return 0
+        var product = this[0]
+        for (element in this.subList(1, this.size)) {
+            product *= element
+        }
+        return product
     }
 
-    fun part2(input: List<String>): Int = input.size
+    fun getNeighbours(rowNum: Int, colNum: Int, rows: List<List<Int>>) =
+        listOf(-1 to 0, 0 to -1, 0 to 1, 1 to 0).mapNotNull { (addRow, addCol) ->
+            val neighbourRow = rowNum + addRow
+            val neighbourCol = colNum + addCol
+
+            if (neighbourRow >= 0 && neighbourRow < rows.size && neighbourCol >= 0 && neighbourCol < rows[0].size)
+                neighbourRow to neighbourCol
+            else
+                null
+        }
+
+    fun getLowPoints(rows: List<List<Int>>): List<Pair<Int, Int>> = rows
+        .mapIndexed { rowNum, values ->
+            values.mapIndexedNotNull { colNum, _ ->
+                val neighbours = getNeighbours(rowNum, colNum, rows)
+                val allNeighboursAreHigher = neighbours
+                    .map { (neighbourRow, neighbourCol) -> rows[neighbourRow][neighbourCol] }
+                    .all { neighbour -> neighbour > rows[rowNum][colNum] }
+
+                if(allNeighboursAreHigher) {
+                    rowNum to colNum
+                }
+                else {
+                    null
+                }
+            }
+        }
+        .flatten()
+
+    fun part1(input: List<String>): Int {
+        val rows = input.map { it.toList().map { it.toString().toInt() } }
+        val lowPoints = getLowPoints(rows)
+        return lowPoints
+            .sumOf { (rowNum, colNum) -> rows[rowNum][colNum] + 1 }
+    }
+
+    fun part2(input: List<String>): Int {
+        val rows = input.map { it.toList().map { it.toString().toInt() } }
+        val lowPoints = getLowPoints(rows)
+
+        return lowPoints.map { (rowNum, colNum) ->
+            var basinSize = 0
+
+            var up = rowNum - 1
+            while (up >= 0 && rows[up][colNum] != 9){
+                basinSize += rows[up][colNum]
+                up--
+            }
+
+            // Look down
+            var down = rowNum + 1
+            while (down < rows.size && rows[down][colNum] != 9){
+                basinSize += rows[down][colNum]
+                down++
+            }
+
+            // Look left
+            var left = colNum - 1
+            while (left >= 0 && rows[rowNum][left] != 9){
+                basinSize += rows[rowNum][left]
+                left--
+            }
+
+            // Look right
+            var right = rowNum + 1
+            while (right < rows.size && rows[rowNum][right] != 9){
+                basinSize += rows[rowNum][right]
+                right++
+            }
+
+            basinSize
+        }
+        .sortedDescending()
+        .subList(0, 3)
+        .product()
+
+    }
 
     val testInput = readInput("${dayName}_test")
     val input = readInput(dayName)
