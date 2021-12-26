2d7d04b8cba9d8c1f7bcf3e20baf84bd414d2cee
Martijn van der Blom <martijn@vanderblom.dev>
2021-12-26 23:11:57 +0100

    Scaffolding day 13

---
+++ src/Day13.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val dayName = "Day13"
+
+    fun part1(input: List<String>): Int = input.size
+
+    fun part2(input: List<String>): Int = input.size
+
+    val testInput = readInput("${dayName}_test")
+    val input = readInput(dayName)
+
+    val testOutputPart1 = part1(testInput)
+    testOutputPart1 isEqualTo 1
+
+    val outputPart1 = part1(input)
+    outputPart1 isEqualTo 1
+
+    val testOutputPart2 = part2(testInput)
+    testOutputPart2 isEqualTo 1
+
+    val outputPart2 = part2(input)
+    outputPart2 isEqualTo 1
+}
